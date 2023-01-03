843a9654ceccfb85d73e4a6ef77d17e03143eab1
Victor Kempfer <kempfervictor@gmail.com>
2023-01-03 19:11:36 -0300

    build: add new configuration and new dependency

--- ignite-aulas/reactjs/01-github-explorer/babel.config.js
+++ ignite-aulas/reactjs/01-github-explorer/babel.config.js
@@ -1,3 +1,24 @@
+const { runtime } = require("webpack");
+
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
+  presets: [
+    "@babel/preset-env",
+    [
+      "@babel/preset-react",
+      {
+        runtime: "automatic",
+      },
+    ],
+  ],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["./src"],
+        alias: {
+          "^@(.+)": "./src/\\1",
+        },
+      },
+    ],
+  ],
 };
